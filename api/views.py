8d9bf054764a42f0e45a73164a0afdff3fa2e683
gambastian <sebas.gamba02@gmail.com>
2016-09-25 11:00:30 -0500

    plantilla bootstrap

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
--- freeSounds/urls.py
+++ freeSounds/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+from api.views import index
+
 urlpatterns = [
+    # url(r'^api/', include('api.views')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
 ]
