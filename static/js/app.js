0beecf992edc6da861b562ef3fb50786fd6ce21f
gambastian <sebas.gamba02@gmail.com>
2016-09-26 00:22:54 -0500

    Ajuste de angular js y prueba del app.js

---
+++ static/js/app.js
@@ -0,0 +1,31 @@
+/**
+ * @name freesounds
+ * @description Aplicacion principal
+ */
+(function () {
+    'use strict';
+
+    var freesounds = angular.module('freesounds', ['ngCookies', 'ngRoute', 'ngResource', 'freesounds.controllers', 'freesounds.services']);
+    var freesoundsControllers = angular.module('freesounds.controllers', []);
+    var freesoundsServices = angular.module('freesounds.services', []);
+
+    var Configuration = function ($routeProvider, $compileProvider, $locationProvider, $httpProvider) {
+        $routeProvider.when('/pieces', {
+            templateUrl: 'static/partials/pieces.html',
+        }).when('/about', {
+            templateUrl: 'static/partials/about.html',
+        }).otherwise({
+            redirectTo: '/pieces'
+        });
+
+        // remueve clases css inecesarias
+        $compileProvider.debugInfoEnabled(true);
+
+        //Anexa por defecto a todas las llamadas el encabezado
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+    };
+
+    angular.module('freesounds').config(['$routeProvider', '$compileProvider', '$locationProvider', '$httpProvider', Configuration]);
+}());
