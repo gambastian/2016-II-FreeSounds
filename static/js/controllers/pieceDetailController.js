719b1d3edfeca8b1bb666d1c4e930e4e24af60a0
gambastian <sebas.gamba02@gmail.com>
2016-10-02 23:58:27 -0500

    formulario para agregar la letra de la cancion

--- static/js/controllers/pieceDetailController.js
+++ static/js/controllers/pieceDetailController.js
@@ -6,10 +6,27 @@
 
     var PieceDetailCrtl = function ($rootScope, $scope, $location, $routeParams, piecesService) {
 
+        $scope.viewLyricsInput = false;
+        $scope.piece = null;
+        $scope.hasLyrics = false;
+        $scope.lyrics = 'Add new lyrics to the piece...';
+
         piecesService.query($routeParams.piece_id).then(function (response) {
             $scope.piece = response[0];
-        })
+            $scope.hasLyrics = $scope.piece.fields.lyrics != null && 0 != $scope.piece.fields.lyrics.trim().length;
+        });
 
+        $scope.toggleLyricsInput = function () {
+            $scope.viewLyricsInput = !$scope.viewLyricsInput;
+        };
+
+        $scope.discardLyrics = function () {
+            $scope.viewLyricsInput = false;
+        };
+
+        $scope.saveLyrics = function (lyrics) {
+            console.log(lyrics);
+        };
     };
 
     angular.module('freesounds.controllers').controller('PieceDetailCrtl', ['$rootScope', '$scope', '$location', '$routeParams', 'piecesService', PieceDetailCrtl]);
