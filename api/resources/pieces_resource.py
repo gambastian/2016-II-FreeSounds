417cad227bd280db2c8ae1a071a7ae76af587adf
odgarcia <od.garcia@uniandes.edu.co>
2016-10-08 21:30:49 -0500

    Edicion de la obra

--- api/resources/pieces_resource.py
+++ api/resources/pieces_resource.py
@@ -1,10 +1,12 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-from django.shortcuts import get_list_or_404
-from django.shortcuts import get_object_or_404
+import json
+
 from django.core import serializers
-from api.models import Piece
-from api.forms import PieceForm
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.shortcuts import get_list_or_404, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+
+from api.models import Piece, Category
 
 ###########################################
 # Resource for operations with Piece class
@@ -21,17 +23,38 @@ def piece_by_id(request, piece_id):
     return HttpResponse(serializers.serialize("json", piece))
 
 @csrf_exempt
-def piece_edit(request, piece_id):
-    piece = get_object_or_404(Piece, pk=piece_id)
+def update_piece(request):
     if request.method == "POST":
-        form = PieceForm(request.POST, instance=piece)
-        if form.is_valid():
-            piece = form.save(commit=False)
-            piece.name = request.name
-            piece.name = request.name
-            piece.save()
-            return HttpResponse(serializers.serialize("json", piece))
-    else:
-        form = PieceForm(instance=piece)
-    return HttpResponse(serializers.serialize("json", piece))
+        jsonPiece = json.loads(request.body)
+        piece_id = jsonPiece['body']['pk']
+        pieces = get_list_or_404(Piece.objects.filter(pk=piece_id))
+        if len(pieces) == 0:
+            return JsonResponse({"mensaje": "There are no pieces with id" + piece_id})
+        else:
+            name = jsonPiece['body']['fields']['name']
+            url = jsonPiece['body']['fields']['url']
+            image_cover = jsonPiece['body']['fields']['image_cover']
+            duration = jsonPiece['body']['fields']['duration']
+            category = jsonPiece['body']['fields']['category']
+            lyrics = jsonPiece['body']['fields']['lyrics']
+
+            selected_piece = pieces[0]
+
+            if name is not None:
+                selected_piece.name = name
+            if url is not None:
+                selected_piece.url = url
+            if image_cover is not None:
+                selected_piece.image_cover = image_cover
+            if duration is not None:
+                selected_piece.duration = duration
+            if category is not None:
+                cat = get_object_or_404(Category, pk = category)
+                selected_piece.category = cat
+            if lyrics is not None:
+                selected_piece.lyrics = lyrics
+
+            selected_piece.save()
+
+            return JsonResponse({"mensaje": "successfully updated"})
 
